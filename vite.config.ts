f4f04291ff8b5b28f43285c227b4957565558e27
TakahashiMasaya <ma.taka.ic@icloud.com>
2024-03-16 18:52:08 +0900

    fixed to build

--- vite.config.ts
+++ vite.config.ts
@@ -6,52 +6,49 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
-export default defineConfig((params) => {
-  return {
-    base: '/digitClock/',
-    plugins: [
-      vue(),
-      vueJsx(),
-      params.command === 'build' &&
-        VitePWA({
-          manifest: {
-            name: 'Digit Clock',
-            short_name: 'Digit Clock',
-            background_color: '#000000',
-            display: 'fullscreen',
-            icons: [
-              {
-                src: 'icons/icon-192x192.png',
-                sizes: '192x192',
-                type: 'image/png'
-              },
-              {
-                src: 'icons/icon-512x512.png',
-                sizes: '512x512',
-                type: 'image/png'
-              }
-            ]
+export default defineConfig({
+  base: '/digitClock/',
+  plugins: [
+    vue(),
+    vueJsx(),
+    VitePWA({
+      manifest: {
+        name: 'Digit Clock',
+        short_name: 'Digit Clock',
+        background_color: '#000000',
+        display: 'fullscreen',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
-          registerType: 'autoUpdate',
-          includeAssets: [
-            'icons/*.{ico,png}',
-            'images/*.webp',
-            'images-dom/*.webp'
-          ],
-          workbox: {
-            // Only precache these files - html should be excluded
-            globPatterns: ['**/*.{js,css}'],
-            // NOTE: index.htmlのrevisionを毎回変更することで、キャッシュを更新する
-            additionalManifestEntries: [
-              { url: 'index.html', revision: `${Date.now()}` }
-            ]
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           }
-        })
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+        ]
+      },
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'icons/*.{ico,png}',
+        'images/*.webp',
+        'images-dom/*.webp'
+      ],
+      workbox: {
+        // Only precache these files - html should be excluded
+        globPatterns: ['**/*.{js,css}'],
+        // NOTE: index.htmlのrevisionを毎回変更することで、キャッシュを更新する
+        additionalManifestEntries: [
+          { url: 'index.html', revision: `${Date.now()}` }
+        ]
       }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
