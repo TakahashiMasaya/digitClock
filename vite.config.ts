72f36a17de0e5f8dd40face9bd58bf57ca99a0fb
TakahashiMasaya <ma.taka.ic@icloud.com>
2024-07-26 13:55:27 +0900

    updated packages

--- vite.config.ts
+++ vite.config.ts
@@ -11,40 +11,41 @@ export default defineConfig(({ command }) => ({
   plugins: [
     vue(),
     vueJsx(),
-    command === 'serve' && VitePWA({
-      manifest: {
-        name: 'Digit Clock',
-        short_name: 'Digit Clock',
-        background_color: '#000000',
-        display: 'fullscreen',
-        icons: [
-          {
-            src: 'icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      registerType: 'autoUpdate',
-      includeAssets: [
-        'icons/*.{ico,png}',
-        'images/*.webp',
-        'images-dom/*.webp'
-      ],
-      workbox: {
-        // Only precache these files - html should be excluded
-        globPatterns: ['**/*.{js,css}'],
-        // NOTE: index.htmlのrevisionを毎回変更することで、キャッシュを更新する
-        additionalManifestEntries: [
-          { url: 'index.html', revision: `${Date.now()}` }
-        ]
-      }
-    })
+    command === 'serve' &&
+      VitePWA({
+        manifest: {
+          name: 'Digit Clock',
+          short_name: 'Digit Clock',
+          background_color: '#000000',
+          display: 'fullscreen',
+          icons: [
+            {
+              src: 'icons/icon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        },
+        registerType: 'autoUpdate',
+        includeAssets: [
+          'icons/*.{ico,png}',
+          'images/*.webp',
+          'images-dom/*.webp'
+        ],
+        workbox: {
+          // Only precache these files - html should be excluded
+          globPatterns: ['**/*.{js,css}'],
+          // NOTE: index.htmlのrevisionを毎回変更することで、キャッシュを更新する
+          additionalManifestEntries: [
+            { url: 'index.html', revision: `${Date.now()}` }
+          ]
+        }
+      })
   ],
   resolve: {
     alias: {
